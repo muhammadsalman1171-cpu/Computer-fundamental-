e772f6defc1a9ec3719b648d7f4887373b4976c7
muhammadsalman1171-cpu <muhammadsalman1171@gmail.com>
2026-03-09 10:44:47 -0700

    Add files via upload

---
+++ Lab tasks 2/LAB_2 TASK_7.cpp
@@ -0,0 +1,18 @@
+// Name: muhammad salman
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0748
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // loop to print name 4 times
+    for (int i = 1; i < 5; i++)
+    {
+        cout << "muhammad salman" << endl;  // print name
+    }
+
+    return 0;  // program ends
+}
