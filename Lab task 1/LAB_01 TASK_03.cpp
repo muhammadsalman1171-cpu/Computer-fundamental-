bc9d0739d5f8996029e40e241578ebd7d35dc364
muhammadsalman1171-cpu <muhammadsalman1171@gmail.com>
2026-03-09 10:24:18 -0700

    Add files via upload

---
+++ Lab task 1/LAB_01 TASK_03.cpp
@@ -0,0 +1,18 @@
+// Name: muhammad salman
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0748
+// Section: A
+
+#include <iostream>   // For input and output
+using namespace std;  // Using standard namespace
+
+int main()
+{
+    // Printing first line with backslash
+    cout << "I am in \\ CS department. \n";
+
+    // Printing second line
+    cout << "I am programming enthusiast!";
+
+    return 0;  // Program ends successfully
+}
