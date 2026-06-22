4663f22d8c5d856081989c9530efd7eeb0f2cc6d
muhammadsalman1171-cpu <muhammadsalman1171@gmail.com>
2026-06-22 09:02:58 +0500

    Add files via upload

---
+++ lab13/lab work/Lab 13 Lab Work 1.cpp
@@ -0,0 +1,26 @@
+// Name: Muhammmad salman
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0748
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Declaring structure Person
+struct Person {
+    string name;
+    int age;
+    string address;
+};
+
+int main() {
+    // Declaring and initializing structure variable person1
+    Person person1 = {"Shahzad faisal", 19, "Pabbi, Khyber Pakhtunkhwa"};
+
+    // Displaying structure members using dot notation
+    cout << "Name: " << person1.name << endl;
+    cout << "Age: " << person1.age << endl;
+    cout << "Address: " << person1.address << endl;
+
+    return 0;
+}
